36e9509aecdb58f9898e3f165b2fde566d73d6f8
YoussefABH <youssef.a.b.h@gmail.com>
2026-06-05 02:39:32 +0100

    Initialize Supabase client and add getCurrentUser function

--- supabase.js
+++ supabase.js
@@ -1,1 +1,12 @@
+const SUPABASE_URL = "https://ecwlqiuvgmikglnfybon.supabase.co";
+const SUPABASE_ANON_KEY = "REMPLACE_PAR_TA_CLE_PUBLISHABLE";
 
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
+
+async function getCurrentUser() {
+    const { data } = await supabaseClient.auth.getUser();
+    return data.user;
+}
