f75f30b9854e98ab48db2b3796240fea58fccebe
YoussefABH <youssef.a.b.h@gmail.com>
2026-06-05 02:55:15 +0100

    Update Supabase client initialization with new key

--- supabase.js
+++ supabase.js
@@ -1,12 +1,8 @@
 const SUPABASE_URL = "https://ecwlqiuvgmikglnfybon.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjd2xxaXV2Z21pa2dsbmZ5Ym9uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1MDEzNDUsImV4cCI6MjA5NjA3NzM0NX0.ppNWQkwIsPLevIcu00a7__oWHgTdVVj3fy9Uv3GRWOs";
+
+const SUPABASE_ANON_KEY = "sb_publishable_ROrM2kw2PBgsYr1xq7Oi4w_pThtprFF";
 
 const supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
 );
-
-async function getCurrentUser() {
-    const { data } = await supabaseClient.auth.getUser();
-    return data.user;
-}
