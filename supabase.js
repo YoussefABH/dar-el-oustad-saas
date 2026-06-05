e92b26b260dbfa0cb0ccf195db89009c6f58a22a
YoussefABH <youssef.a.b.h@gmail.com>
2026-06-05 02:49:12 +0100

    Change Supabase anonymous key
    Updated the Supabase anonymous key to a new value.

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://ecwlqiuvgmikglnfybon.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_ROrM2kw2PBgsYr1xq7Oi4w_pThtprFF";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVjd2xxaXV2Z21pa2dsbmZ5Ym9uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1MDEzNDUsImV4cCI6MjA5NjA3NzM0NX0.ppNWQkwIsPLevIcu00a7__oWHgTdVVj3fy9Uv3GRWOs";
 
 const supabaseClient = supabase.createClient(
     SUPABASE_URL,
