484ceb9d819ec34e9abd987a23e714c2c1eab3c4
YoussefABH <youssef.a.b.h@gmail.com>
2026-06-06 23:52:58 +0100

    Update router.js

--- js/core/router.js
+++ js/core/router.js
@@ -2,7 +2,7 @@ const routes = {
     dashboard: 'js/modules/dashboard/dashboard.js',
     students: 'js/modules/students/students.js',
     teachers: 'js/modules/teachers/teachers.js',
-    groups: 'js/modules/groups/groups.js',
+    groups: 'js/modules/groups/groups.js',      // <-- ajout
     attendance: 'js/modules/attendance/attendance.js',
     payments: 'js/modules/payments/payments.js',
     expenses: 'js/modules/expenses/expenses.js',
