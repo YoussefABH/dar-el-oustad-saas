3b96002eda1b0bc0c7cc5d08ff65c9c324cfc39d
YoussefABH <youssef.a.b.h@gmail.com>
2026-06-07 19:26:37 +0100

    Refactor escapeHtml and showAlert functions

--- js/utils/dom.js
+++ js/utils/dom.js
@@ -1,11 +1,27 @@
-export function escapeHtml(str = '') {
-    return String(str).replace(/[&<>"']/g, m => ({ '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#39;' }[m]));
+export function escapeHtml(str) {
+    if (!str) return '';
+    return String(str).replace(/[&<>"']/g, m => ({
+        '&': '&amp;', '<': '&lt;', '>': '&gt;', '"': '&quot;', "'": '&#39;'
+    }[m]));
 }
 
-export function showAlert(message, type = 'error') {
+export function showAlert(message, type = 'success') {
     const toast = document.createElement('div');
     toast.className = `alert-toast alert-${type}`;
     toast.textContent = message;
     document.body.appendChild(toast);
-    setTimeout(() => toast.remove(), 4000);
+    setTimeout(() => toast.remove(), 3500);
+}
+
+export async function withLoading(button, callback) {
+    const originalText = button.textContent;
+    const originalDisabled = button.disabled;
+    button.disabled = true;
+    button.innerHTML = '<span class="spinner-small"></span> Chargement...';
+    try {
+        await callback();
+    } finally {
+        button.disabled = originalDisabled;
+        button.textContent = originalText;
+    }
 }
