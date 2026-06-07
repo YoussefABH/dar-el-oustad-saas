3a33441363390ed68ce01c213d08f43d8c8c5919
YoussefABH <youssef.a.b.h@gmail.com>
2026-06-07 12:37:05 +0100

    Refactor Header class to improve render method

--- js/components/header.js
+++ js/components/header.js
@@ -1,18 +1,44 @@
+// Header.js
+
 export class Header {
-    constructor(centerName, userEmail) {
+
+    constructor(centerName = 'Dar El-Oustad', userEmail = '') {
         this.centerName = centerName;
         this.userEmail = userEmail;
     }
 
     render() {
-        const div = document.createElement('div');
-        div.className = 'header-content';
-        div.innerHTML = `
-            <div class="header-title">${this.centerName}</div>
-            <div class="header-center-info">${this.userEmail}</div>
-            <button id="logout-header-btn" class="btn btn-sm">Déconnexion</button>
+
+        const header = document.createElement('div');
+
+        header.className = 'header-content';
+
+        header.innerHTML = `
+            <div class="header-title">
+                ${this.centerName}
+            </div>
+
+            <div class="header-center-info">
+                ${this.userEmail}
+            </div>
+
+            <button
+                id="logout-header-btn"
+                class="btn btn-sm">
+                Déconnexion
+            </button>
         `;
-        div.querySelector('#logout-header-btn').onclick = () => window.logout?.();
-        return div;
+
+        const logoutBtn =
+            header.querySelector('#logout-header-btn');
+
+        logoutBtn.addEventListener('click', () => {
+
+            if (typeof window.logout === 'function') {
+                window.logout();
+            }
+        });
+
+        return header;
     }
 }
